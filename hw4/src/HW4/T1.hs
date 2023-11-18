8eb2fb8de7986714068860ebd9ec435c04b627db
imorda <btimka1@gmail.com>
2023-11-18 08:49:27 +0300

    + Haddock

--- hw4/src/HW4/T1.hs
+++ hw4/src/HW4/T1.hs
@@ -2,43 +2,63 @@
 {-# LANGUAGE LambdaCase     #-}
 
 module HW4.T1
-  ( EvaluationError (..)
-  , ExceptState (..)
-  , mapExceptState
-  , wrapExceptState
-  , joinExceptState
-  , modifyExceptState
-  , throwExceptState
-  , eval
+  (
+  -- * Type
+  -- | This module exposes custom types encapsulating the parser information.
+    EvaluationError (..),
+    ExceptState (..),
+
+  -- * ExceptState monad functionality
+  -- | Functions transforming or working with ExceptState
+    mapExceptState,
+    wrapExceptState,
+    joinExceptState,
+    modifyExceptState,
+    throwExceptState,
+
+  -- *  Evaluator
+  -- | Evaluates an expression represented as 'Expr' type
+    eval
   ) where
 
 import Control.Monad (ap)
 import HW4.Types
 
+-- | The 'ExceptState' data type encapsulates stateful computations which can fail with an 'e' error,
+-- altering an 's' state, and producing a value of type 'a' when successful
 newtype ExceptState e s a = ES {runES :: s -> Except e (Annotated s a)}
 
+-- | 'mapAnnotated' transforms the value within 'Annotated' keeping error as it is.
 mapAnnotated :: (a -> b) -> (Annotated e a -> Annotated e b)
 mapAnnotated f (a :# e) = f a :# e
 
+-- | 'mapExcept' transforms the successful value within 'Except', keeping error as it is.
 mapExcept :: (a -> b) -> (Except e a -> Except e b)
 mapExcept _ (Error e)   = Error e
 mapExcept f (Success a) = Success $ f a
 
+-- | 'wrapExceptState' lifts a value into an 'ExceptState' computation, discarding the old state.
 mapExceptState :: (a -> b) -> ExceptState e s a -> ExceptState e s b
 mapExceptState f (ES runA) = ES $ \s -> mapExcept (mapAnnotated f) $ runA s
 
+-- | 'wrapExceptState' lifts a value into an 'ExceptState' computation,
+--   turning it into a computation that always successfully returns that value
+--   without changing the state.
 wrapExceptState :: a -> ExceptState e s a
 wrapExceptState a = ES $ \s -> Success $ a :# s
 
+-- | 'joinExceptState' transforms a nested ExceptState computation into a single-layered one.
 joinExceptState :: ExceptState e s (ExceptState e s a) -> ExceptState e s a
 joinExceptState (ES runOuter) = ES $ \s ->
   case runOuter s of
     Error e                         -> Error e
     Success (ES runInner :# outerS) -> runInner outerS
 
+-- | 'modifyExceptState' applies a function to the state within an 'ExceptState' computation.
 modifyExceptState :: (s -> s) -> ExceptState e s ()
 modifyExceptState f = ES $ \s -> Success $ () :# f s
 
+-- | Throws user-defined 'EvaluationError' error for handling expressions like division by zero.
 throwExceptState :: e -> ExceptState e s a
 throwExceptState e = ES $ \_ -> Error e
 
@@ -52,9 +72,12 @@ instance Applicative (ExceptState e s) where
 instance Monad (ExceptState e s) where
   (>>=) stateA f = joinExceptState $ mapExceptState f stateA
 
+-- | Represents possible errors during evaluation
 data EvaluationError = DivideByZero
   deriving Show
 
+-- | 'evalBinaryExcept' represent evaluator for binary operations.
+-- It further checks for exceptions like division by zero.
 evalBinaryExcept :: (Double -> Double -> Double) ->
               (Double -> Double -> Prim Double) ->
               Expr -> Expr ->
@@ -67,6 +90,7 @@ evalBinaryExcept evalF logF lhsExpr rhsExpr checkExcept = do
   modifyExceptState (logF lhsVal rhsVal :)
   return $ evalF lhsVal rhsVal
 
+-- | evalUnary represent evaluator for unary operations.
 evalUnary :: (Double -> Double) ->
               (Double -> Prim Double) ->
               Expr -> ExceptState EvaluationError [Prim Double] Double
@@ -75,12 +99,15 @@ evalUnary evalF logF expr = do
   modifyExceptState (logF val :)
   return $ evalF val
 
+-- | 'dummyState' does nothing, preserving the state
 dummyState :: ExceptState EvaluationError [Prim Double] ()
 dummyState = modifyExceptState id
 
+-- | 'noCheck' is a simple operation validator that checks nothing
 noCheck :: Double -> Double -> ExceptState EvaluationError [Prim Double] ()
 noCheck _ _ = dummyState
 
+-- | 'eval' is the main evaluator which recursively evaluates each Expr to Double.
 eval :: Expr -> ExceptState EvaluationError [Prim Double] Double
 eval (Val x)                    = return x
 eval (Op (Add lhsExpr rhsExpr)) = evalBinaryExcept (+) Add lhsExpr rhsExpr noCheck
--- hw4/src/HW4/T2.hs
+++ hw4/src/HW4/T2.hs
@@ -2,11 +2,18 @@
 {-# LANGUAGE GeneralisedNewtypeDeriving #-}
 
 module HW4.T2
-  ( ParseError (..)
-  , runP
-  , pChar
-  , parseError
-  , parseExpr
+  (
+  -- * Type
+  -- | Custom data types encapsulating the parser information.
+    ParseError (..),
+    Parser,
+
+  -- * Parsers
+  -- | Functions exporting combinatory parsing techniques which can also fail with 'ParseError'.
+    runP,
+    pChar,
+    parseError,
+    parseExpr,
   ) where
 
 import Control.Applicative
@@ -19,23 +26,36 @@ import Numeric.Natural (Natural)
 import HW4.T1 (ExceptState (..))
 import HW4.Types
 
+-- | Captures any parsing error along with the position of
+--   the error in the input.
 newtype ParseError = ErrorAtPos Natural
   deriving Show
 
+-- | 'Parser' is a simple wrapper over 'ExceptState' with 'ParseError'
+--   as the error type, meaning that any computation made in the
+--   'Parser' monad has the ability to fail with a 'ParseError'.
+--   It models a computation that consumes a string and produces a value,
+--   and it can either succeed, returning the parsed value and
+--   the rest of the string, or fail with an error.
 newtype Parser a = P (ExceptState ParseError (Natural, String) a)
   deriving newtype (Functor, Applicative, Monad)
 
+-- | Runs a parser on a given input string. If parsing is successful, the result is returned,
+-- otherwise an error is thrown.
 runP :: Parser a -> String -> Except ParseError a
 runP (P (ES runA)) input = case runA (0, input) of
   Error e          -> Error e
   Success (a :# _) -> Success a
 
+-- | Parses a single character. If parsing is successful, the character is returned,
+-- otherwise an error is thrown.
 pChar :: Parser Char
 pChar = P $ ES $ \(pos, s) ->
   case s of
     []     -> Error (ErrorAtPos pos)
     (c:cs) -> Success (c :# (pos + 1, cs))
 
+-- | A parser that always fails with an ErrorAtPos error at the current position.
 parseError :: Parser a
 parseError = P $ ES $ \(pos, _) ->
   Error (ErrorAtPos pos)
@@ -49,28 +69,36 @@ instance Alternative Parser where
 
 instance MonadPlus Parser
 
+-- | Expects the end of file (EOF) in the input. If EOF is found, a unit (()) is returned,
+-- otherwise an error is thrown.
 pEof :: Parser ()
 pEof = P $ ES $ \input@(pos, s) ->
   case s of
     [] -> Success (() :# input)
     _  -> Error (ErrorAtPos pos)
 
+-- | Expects a specific character in the input. If the character is found, it's returned,
+-- otherwise an error is thrown.
 pExpect :: Char -> Parser Char
 pExpect x = mfilter (== x) pChar
 
+-- | Parses a sequence of digits.
 pDigits :: Num a => Parser [a]
 pDigits = do
   digitStr <- some $ mfilter isDigit pChar
   return $ map (fromIntegral . digitToInt) digitStr
 
+-- | Parses an integer number.
 pInteger :: Parser Integer
 pInteger = pDigits >>= \digits ->
   return $ foldl1 (\a b -> a * 10 + b) digits
 
+-- | Parses an integer and counts the number of digits it contains.
 pIntegerCounting :: Parser (Integer, Natural)
 pIntegerCounting = pDigits >>= \digits ->
   return $ foldl (\(a, counter) b -> (a * 10 + b, counter + 1)) (0, 0) digits
 
+-- | Parses a double number. A double is represented as an integer part and a decimal part.
 pDouble :: Parser Double
 pDouble = do
   digitsInt <- pInteger
@@ -82,9 +110,11 @@ pDouble = do
     (digitsInt * (10 ^ fracLength) + fracInt)
     $ - fromIntegral fracLength
 
+-- | Skips all whitespace characters in the input.
 pSkipWs :: Parser ()
 pSkipWs = void $ many $ mfilter isSpace pChar
 
+-- Grammar schema
 -- E -> TE'
 -- E' -> S+STE'
 -- E' -> S-STE'
@@ -101,11 +131,15 @@ pSkipWs = void $ many $ mfilter isSpace pChar
 -- S -> ⎵S
 -- S -> eps
 
+-- | This function is used for representing the grammar of a parser,
+-- representing expression
 parseE :: Parser Expr
 parseE = do
   lhs <- parseT
   parseE' lhs
 
+-- | This unction is used for representing the grammar of a parser,
+-- representing expression part
 parseE' :: Expr -> Parser Expr
 parseE' lhs =
   (do
@@ -117,12 +151,16 @@ parseE' lhs =
       else Op . Sub lhs <$> parseT) <|>
   return lhs
 
+-- | This function is used for representing the grammar of a parser,
+-- representing term
 parseT :: Parser Expr
 parseT = do
   lhs <- parseF
   parseT' lhs
 
 
+-- | This function is used for representing the grammar of a parser,
+-- representing term part
 parseT' :: Expr -> Parser Expr
 parseT' lhs =
   (do
@@ -134,6 +172,7 @@ parseT' lhs =
       else Op . Div lhs <$> parseF) <|>
   return lhs
 
+-- | Parses a factor. A factor is either a number or an expression within parentheses.
 parseF :: Parser Expr
 parseF =
   (do
@@ -149,6 +188,8 @@ parseF =
     pSkipWs
     return $ Val n)
 
+-- | 'parseExpr' parses a mathematical expression represented as a string into an 'Expr' data structure.
+-- If the parsing is successful, an 'Expr' is returned, otherwise an error is thrown.
 parseExpr :: String -> Except ParseError Expr
 parseExpr = runP $ do
   parsed <- parseE
